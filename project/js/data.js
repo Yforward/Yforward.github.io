883f9cdb328c1615d5aa4268b2648858adb6d955
Yforward <1067536941@qq.com>
2016-09-05 22:03:37 +0800

    shijue

---
+++ project/js/data.js
@@ -0,0 +1,39 @@
+var dataTop=[{
+		img:['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg','img/5.jpg','img/6.jpg','img/7.jpg','img/8.jpg','img/9.jpg','img/10.jpg'],
+		title:['General Electric','Alphadog','Montblanc','Steinway & Sons','Spendenland','Aria','AURIGA','CCT','Flatex','congstar'],
+		tips:['Print','Website','Eventdesign','Website','Website','Corporate Identity','Corporate Identity','Corporate Identity','Website','Website']
+		
+	},{
+		img:['img/21.jpg','img/22.jpg','img/23.jpg','img/24.jpg','img/25.jpg','img/26.jpg','img/27.jpg','img/28.jpg','img/29.jpg','img/30.jpg'],
+		title:['General Electric','Alphadog','Montblanc','Steinway & Sons','Spendenland','Aria','AURIGA','CCT','Flatex','congstar'],
+		tips:['Print','Website','Eventdesign','Website','Website','Corporate Identity','Corporate Identity','Corporate Identity','Website','Website']
+		
+	},{
+		img:['img/41.jpg','img/42.jpg','img/43.jpg','img/44.jpg','img/45.jpg','img/46.jpg','img/47.jpg','img/48.jpg','img/49.jpg','img/50.jpg'],
+		title:['General Electric','Alphadog','Montblanc','Steinway & Sons','Spendenland','Aria','AURIGA','CCT','Flatex','congstar'],
+		tips:['Print','Website','Eventdesign','Website','Website','Corporate Identity','Corporate Identity','Corporate Identity','Website','Website']
+	},{
+		img:['img/61.jpg','img/62.jpg','img/63.jpg','img/64.jpg','img/65.jpg','img/66.jpg','img/67.jpg','img/68.jpg','img/69.jpg','img/70.jpg'],
+		title:['General Electric','Alphadog','Montblanc','Steinway & Sons','Spendenland','Aria','AURIGA','CCT','Flatex','congstar'],
+		tips:['Print','Corporate Design','Web','Corporate Design','Print','Web','Print','Print','Web','Web']
+	}]
+var dataBottom=[{
+		img:['img/11.jpg','img/12.jpg','img/13.jpg','img/14.jpg','img/15.jpg','img/16.jpg','img/17.jpg','img/18.jpg','img/19.jpg','img/20.jpg'],
+		title:['Frobusch','IP','Check My Car','Edel','myTaxi','Bifi','ULI SCHNEIDER','Wizard','NORDIC OIL','Elbweiss'],
+		tips:['Corporate Design','Corporate Identity','Corporate Identity','Website','Corporate Identity','Eventdesign','Print','Corporate Design','Print','Corporate Identity']
+	},{
+		img:['img/31.jpg','img/32.jpg','img/33.jpg','img/34.jpg','img/35.jpg','img/36.jpg','img/37.jpg','img/38.jpg','img/39.jpg','img/40.jpg'],
+		title:['Frobusch','IP','Check My Car','Edel','myTaxi','Bifi','ULI SCHNEIDER','Wizard','NORDIC OIL','Elbweiss'],
+		tips:['Web','Corporate','Web','Corporate','Web','Corporate','Web','Corporate','Corporate','Corporate']
+		
+	},{
+		img:['img/51.jpg','img/52.jpg','img/53.jpg','img/54.jpg','img/55.jpg','img/56.jpg','img/57.jpg','img/58.jpg','img/59.jpg','img/60.jpg'],
+		title:['Frobusch','IP','Check My Car','Edel','myTaxi','Bifi','ULI SCHNEIDER','Wizard','NORDIC OIL','Elbweiss'],
+		tips:['Corporate','Web','Corporate','Corporate','Web','Corporate','Web','Corporate','Web','Web']
+	},{
+		img:['img/71.jpg','img/72.jpg','img/73.jpg','img/74.jpg','img/75.jpg','img/76.jpg','img/77.jpg','img/78.jpg','img/79.jpg','img/80.jpg'],
+		title:['Frobusch','IP','Check My Car','Edel','myTaxi','Bifi','ULI SCHNEIDER','Wizard','NORDIC OIL','Elbweiss'],
+		tips:['Web','Corporate','Corporate','Web','Corporate','Web','Web','Corporate','Web','Corporate']
+	}];
+var dataImg=['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg','img/5.jpg','img/6.jpg','img/7.jpg','img/8.jpg','img/9.jpg','img/10.jpg','img/11.jpg','img/12.jpg','img/13.jpg','img/14.jpg','img/15.jpg','img/16.jpg','img/17.jpg','img/18.jpg','img/19.jpg','img/20.jpg']
+
